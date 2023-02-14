686284411ccb42ed165648b5e2aeda88e143c3ac
Hampus Kraft <hello@hampuskraft.com>
2023-02-14 13:16:45 +0100

    Set `traces_sample_rate` to 0%

--- wumpus/main.py
+++ wumpus/main.py
@@ -10,7 +10,7 @@ from werkzeug.exceptions import HTTPException
 from wumpus.sanitizer import Sanitizer, SanitizeSchema
 
 SENTRY_DSN = os.environ.get("SENTRY_DSN")
-sentry_sdk.init(SENTRY_DSN, integrations=[FlaskIntegration()], traces_sample_rate=0.25)
+sentry_sdk.init(SENTRY_DSN, integrations=[FlaskIntegration()])
 
 app = Flask(__name__)
 app.config["JSON_SORT_KEYS"] = False
